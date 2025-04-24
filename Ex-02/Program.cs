c3853cd243a6bfb35d5582cb5ad4dce94305c559
Iago00008 <iagofazevedo.08@gmail.com>
2025-04-24 08:39:53 -0300

    02

---
+++ Ex-02/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Digite sua idade:");
+        int idade = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Você tem carteira de motorista? (s/n):");
+        string resposta = Console.ReadLine().ToLower();
+        bool Carteira = resposta == "s";
+
+        bool podeDirigir = idade >= 18 && Carteira;
+
+        if (podeDirigir)
+        {
+            Console.WriteLine("Pode dirigir.");
+        }
+        else
+        {
+            Console.WriteLine("Não pode dirigir.");
+        }
+
+
+   }
+}
