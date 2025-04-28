4b9ca1508c5872b4ef693c96d48cb865369368bf
Iago00008 <iagofazevedo.08@gmail.com>
2025-04-28 09:07:21 -0300

    28

---
+++ exemplos/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int valor = 1;
+
+        switch (valor)
+        {
+            case 1:
+                Console.WriteLine("Valor digitado foi 1");
+                break;
+                case 2:
+                Console.WriteLine("Valor digitado foi 2");
+                break;
+                case 3:
+                Console.WriteLine("Valor não digitado.");
+                break;
+        }
+    }
+}
